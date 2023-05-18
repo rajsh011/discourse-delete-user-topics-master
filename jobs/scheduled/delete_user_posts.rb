1e266873cedd540bc6e1c2ef20680ee02afcdd96
rajsh011 <rajsh011@gmail.com>
2023-05-18 19:39:38 +0530

    Update delete_user_posts.rb

--- jobs/scheduled/delete_user_posts.rb
+++ jobs/scheduled/delete_user_posts.rb
@@ -6,22 +6,38 @@ module Jobs
 
     def execute(args)
       return unless SiteSetting.delete_user_topics_enabled?
+
       username = SiteSetting.delete_posts_for_username
-      batch = SiteSetting.delete_posts_in_single_batch
-      return unless username.present? && batch.to_i > 0 
+      posts_per_batch = SiteSetting.delete_posts_in_single_batch.to_i
+
+      return unless username.present? && posts_per_batch.positive?
 
       user = User.find_by(username: username)
       return unless user.present?
 
-      user.posts.order(created_at: :asc).each_slice(batch.to_i) do |posts|
-        posts.each do |post|
-          if SiteSetting.delete_user_topics_dry_run?
-            Rails.logger.error("DeleteUserPosts would remove Post ID #{post.id} (#{post.topic.title} - #{post.excerpt}) (dry run mode)")
-          else
-            Rails.logger.error("DeleteUserPosts removing Post ID #{post.id} (#{post.topic.title} - #{post.excerpt})")
+      posts = user.posts.order(created_at: :asc)
+      return if posts.empty?
+
+      deleted_count = 0
+      posts.each do |post|
+        break if deleted_count >= posts_per_batch
+
+        if SiteSetting.delete_user_topics_dry_run?
+          Rails.logger.error("DeleteUserPosts would remove Post ID #{post.id} (#{post.topic.title} - #{post.excerpt}) (dry run mode)")
+        else
+          Rails.logger.error("DeleteUserPosts removing Post ID #{post.id} (#{post.topic.title} - #{post.excerpt})")
+          begin
             PostDestroyer.new(Discourse.system_user, post).destroy
+            deleted_count += 1
+          rescue StandardError => e
+            Rails.logger.error("Error deleting post ID #{post.id}: #{e.message}")
           end
         end
+      end
+
+      # Cancel the scheduled job if there are no more posts remaining
+      if posts.size <= posts_per_batch
+        self.class.cancel_scheduled_job
       end
     end
   end
