61d78f55a57c768df5ccc2b9427b5467f2b8b48c
rajsh011 <rajsh011@gmail.com>
2023-06-12 19:15:06 +0530

    changes

--- jobs/scheduled/delete_user_posts.rb
+++ jobs/scheduled/delete_user_posts.rb
@@ -15,29 +15,24 @@ module Jobs
       user = User.find_by(username: username)
       return unless user.present?
 
-      posts = user.posts.order(created_at: :asc)
+      posts = user.posts.limit(posts_per_batch)
 
-      deleted_count = 0
+       # Cancel the scheduled job if there are no more posts remaining
+      if posts.size < 100
+        self.class.cancel_scheduled_job
+      end
       posts.each do |post|
-        break if deleted_count >= posts_per_batch
-
         if SiteSetting.delete_user_topics_dry_run?
           Rails.logger.error("DeleteUserPosts would remove Post ID #{post.id} (#{post.topic.title} - #{post.excerpt}) (dry run mode)")
         else
           Rails.logger.error("DeleteUserPosts removing Post ID #{post.id} (#{post.topic.title} - #{post.excerpt})")
           begin
             PostDestroyer.new(Discourse.system_user, post).destroy
-            deleted_count += 1
           rescue StandardError => e
             Rails.logger.error("Error deleting post ID #{post.id}: #{e.message}")
           end
         end
-      end
-
-      # Cancel the scheduled job if there are no more posts remaining
-      if posts.size <= posts_per_batch
-        self.class.cancel_scheduled_job
-      end
+      end  
     end
   end
 end
